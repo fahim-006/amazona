87d4ca46d29ffe4c996849c08feed97b49060de7
Mashrur Hossain Khan <mashrurhossainkhan@Mashrurs-MacBook-Pro.local>
2020-12-28 13:02:50 +0600

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,64 @@
+const data = {
+    products:[
+        {
+        _id:'1',
+        name: 'Nike slim Skirt',
+        category: 'Shirts',
+        image: 'images/p1.jpg',
+        price: '120',
+        brand: 'Nike',
+        rating: '4.5',
+        numReviews: '10',
+        description: 'high quality shirts',
+        },
+
+        {
+            _id:'2',
+            name: 'Nike slim Pant',
+            category: 'Pants',
+            image: 'images/p2.jpg',
+            price: '150',
+            brand: 'Nike',
+            rating: '4.0',
+            numReviews: '12',
+            description: 'high quality Pants',
+        },
+
+        {
+            _id:'3',
+            name: 'Nike Skirt',
+            category: 'Shirts',
+            image: 'images/p3.jpg',
+            price: '140',
+            brand: 'Nike',
+            rating: '5',
+            numReviews: '100',
+            description: 'high quality shirts',
+        },
+
+        {
+            _id:'4',
+            name: 'slim Skirt',
+            category: 'Shirts',
+            image: 'images/p4.jpg',
+            price: '1200',
+            brand: 'Nike',
+            rating: '4',
+            numReviews: '101',
+            description: 'high quality shirts',
+        },
+
+        {
+            _id:'5',
+            name: 'Skirt',
+            category: 'Shirt',
+            image: 'images/p5.jpg',
+            price: '120',
+            brand: 'Nike',
+            rating: '4.5',
+            numReviews: '109',
+            description: 'high quality shirts',
+        },
+    ],
+};
+export default data;
