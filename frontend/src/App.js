7cca8d3cc81db97e0677c49cfa9d4968a9852d90
Mashrur Hossain Khan <mashrurhossainkhan@Mashrurs-MacBook-Pro.local>
2020-12-28 18:12:20 +0600

    including product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import data from './data'
+import {BrowserRouter, Route} from 'react-router-dom'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
+    <BrowserRouter>
+
+    <div className="grid-container">
+    <header className="row">
       <div>
-        <a class="brand" href="/">amazona</a>
+        <a className="brand" href="/">amazona</a>
       </div>
       <div>
         <a href="/cart">Cart</a>
@@ -12,34 +17,12 @@ function App() {
       </div>
     </header>
     <main>
-      <div>
-        <div className="row center">
-            {data.products.map((product)=>(
-                <div key={product._id} class="card">
-                  <a href={`/product/${product._id}`} >
-                    <img class="medium" src={product.image} alt={product.name} />
-                  </a>
-
-                  <div class="card-body">
-                    <a href={`/product/${product._id}`} >
-                      <h2>{product.name}</h2>
-                    </a>
-                  <div class="rating">
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star"></i> </span>
-                            <span> <i class="fa fa-star-half-o"></i> </span>
-                  </div>
-                  <div class="price">${product.price}</div>
-                </div>
-              </div>
-          ))}
-        </div>
-      </div>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      <Route path="/" component={HomeScreen} exact></Route>
     </main>
-    <footer class="row center">All right reserved</footer>
+    <footer className="row center">All right reserved</footer>
   </div>
+  </BrowserRouter>
   );
 }
 
